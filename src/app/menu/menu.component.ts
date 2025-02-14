cf7d8f094cbdcfc32dd15782249cdebac9adb890
MathDev96 <camilooliveiradev@gmail.com>
2025-02-14 15:57:05 -0300

    adiciona componente de menu com estilos e funcionalidade de abertura/fechamento

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
-import { ProfileService } from '../services/profile.service';
+import { MenuService } from '../services/menu.service';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
+  constructor(private menuService: MenuService) { }
 
-  constructor (private profileService: ProfileService) {}
+  menuStatus = 'closed';
 
+  menuItems?: Array<Menu>;
+
+  menuClick() {
+
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open')
+  }
+
+
+
+  ngOnInit() {
+    const menu = this.menuService.getItems();
+
+    this.menuItems = menu;
+
+  }
 }
